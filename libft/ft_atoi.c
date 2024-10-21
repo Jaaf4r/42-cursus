292e7b24c0341e06af73fabfe20e04c3b2e2d62c
Jaafar Bouhnine <jabouhni@c2r9p13.1337.ma>
2024-10-21 20:15:23 +0100

    start

---
+++ libft/ft_atoi.c
@@ -0,0 +1,32 @@
+int	ft_atoi(const char *str)
+{
+	int	i = 0;
+	int	sign = 1;
+	int	num = 0;
+
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		num = num * 10 + str[i] - 48;
+		i++;
+	}
+	return (num * sign);
+}
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main()
+{
+	printf("%d\n", atoi("    -123abc"));
+	printf("%d\n", ft_atoi("    -123abc"));
+}
+*/
