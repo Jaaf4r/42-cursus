5ecc1d0145c793c5725bb04d243d0080f47c2b6f
Jaaf4r <jaafar.bouhnine@gmail.com>
2025-01-25 12:31:48 +0100

    nice

--- c2/push_swap/main.c
+++ c2/push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: jabouhni <jabouhni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 22:26:00 by jabouhni          #+#    #+#             */
-/*   Updated: 2025/01/25 10:12:34 by jabouhni         ###   ########.fr       */
+/*   Updated: 2025/01/25 12:27:17 by jabouhni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,13 @@ static void	main_conditions(t_node **stack_a, t_node **stack_b)
 	int	size;
 
 	size = ft_lstsize(*stack_a);
-	if (size <= 3)
+	if (is_reversed(*stack_a))
+	{
+		sa(stack_a);
+		write(1, "sa\n", 3);
+		return ;
+	}
+	if (size == 3)
 		sort_three(stack_a);
 	else if (size <= 5)
 		sort_five(stack_a, stack_b);
@@ -73,5 +79,7 @@ int	main(int ac, char **av)
 	stack_b = NULL;
 	if (!is_sorted(stack_a))
 		main_conditions(&stack_a, &stack_b);
+	if (is_sorted(stack_a))
+		write(1, "Ws in the chat\n", 15);
 	return (free_stack(stack_a), 0);
 }
