36badc64f06a48564d8f72ae7470e50da469859f
Jaaf4r <jaafar.bouhnine@gmail.com>
2025-02-22 14:28:32 +0100

    nice

--- c2/so_long/so_long.c
+++ c2/so_long/so_long.c
@@ -75,7 +75,7 @@ int	valid_rectangular_size(t_game *tool)
 	}
 	tool->line_length = (int)ft_strlen(tool->map_2d[0]);
 	if (tool->line_count >= tool->line_length)
-		return (printf("line count: %d | line length: %d\n", tool->line_count, tool->line_length), ft_putstr_fd("Error\nMap shape is not a rectangle", 2), 0);
+		return (ft_putstr_fd("Error\nMap shape is not a rectangle", 2), 0);
 	return (1);
 }
 
@@ -95,51 +95,53 @@ int	up_down_walls(char *s)
 	return (1);
 }
 
-int	check_map_characters(t_game *tool, char *s, t_flag *flags)
+int	check_map_characters(t_game *tool, char *s, t_count *counter)
 {
 	int	i;
 
-	(i = 1), flags->p_flag = 0, flags->e_flag = 0, flags->c_flag = 2;
+	i = 1;
 	while (i < tool->line_length - 1)
 	{
-		if (flags->p_flag == 0 && s[i] == 'P')
-			flags->p_flag = 1;
-		else if (flags->e_flag == 0 && s[i] == 'E')
-			flags->e_flag = 1;
+		if (s[i] == 'P')
+			counter->p_count++;
+		else if (s[i] == 'E')
+			counter->e_count++;
 		else if (s[i] == 'C')
-			flags->c_flag = 1;
-		else if (s[i] == '1' || s[i] == '0')
-			mazal hna khawi
-		// else if (flags->p_flag == 1 && tool->map_2d == 'P'
-		// 	|| flags->e_flag == 1 && tool->map_2d == 'E')
-		// 	return (0);
-		else
-			return (0);
+			counter->c_count++;
+		else if (s[i] != 'P' && s[i] != 'E' && s[i] != 'C' && s[i] != '1'
+			&& s[i] != '0')
+			return (ft_putstr_fd("Error\nMap contains an unknown character\n", 2)
+				, 0);
 		i++;
 	}
 	return (1);
 }
 
-int	check_walls(t_game *tool, t_flag *flags)
+int	check_walls(t_game *tool, t_count *counter)
 {
 	int	i;
 
-	i = 0;
 	if (!up_down_walls(tool->map_2d[0])
 		|| !up_down_walls(tool->map_2d[tool->line_count - 1]))
-		return (0);
-	while (tool->map_2d[i] && i < tool->line_count - 2)
+		return (ft_putstr_fd("Error\nMissing wall\n", 2), 0);
+	counter->p_count = 0;
+	counter->e_count = 0;
+	counter->c_count = 0;
+	i = 1;
+	while (tool->map_2d[i])
 	{
-		if (tool->map_2d[i][0] != '1' || tool->map_2d[i][tool->line_length - 1] != '1'
-			|| !check_map_characters(tool, tool->map_2d[i], flags))
+		if (tool->map_2d[i][0] != '1'
+			|| tool->map_2d[i][tool->line_length - 1] != '1'
+			|| !check_map_characters(tool, tool->map_2d[i], counter))
 			return (0);
 		i++;
 	}
-	exit(1);
+	if (counter->p_count != 1 || counter->e_count != 1 || counter->c_count < 1)
+		return (ft_putstr_fd("Error\nInvalid map\n", 2), 0);
 	return (1);
 }
 
-int	valid_map_file(char *map, t_game *tool, t_flag *flags)
+int	valid_map_file(char *map, t_game *tool, t_count *counter)
 {
 	count_file_lines(map, tool);
 	if (tool->line_count < 3)
@@ -149,7 +151,7 @@ int	valid_map_file(char *map, t_game *tool, t_flag *flags)
 	}
 	tool->map_2d = store_map(map, tool);
 	if (!tool->map_2d || !valid_rectangular_size(tool)
-		|| !check_walls(tool, flags))
+		|| !check_walls(tool, counter))
 		return (0);
 	return (1);
 }
@@ -178,9 +180,10 @@ int	valid_map_name(char *map)
 int	main(int ac, char **av)
 {
 	t_game	tool;
-	t_flag	flags;
+	t_count	counter;
 
-	if (ac != 2 || !valid_map_name(av[1]) || !valid_map_file(av[1], &tool, &flags))
+	if (ac != 2 || !valid_map_name(av[1])
+		|| !valid_map_file(av[1], &tool, &counter))
 		return (1);
 	ft_putstr_fd("NOICE HEHE\n", 1);
 }
--- c2/so_long/so_long.h
+++ c2/so_long/so_long.h
@@ -18,19 +18,19 @@ typedef struct s_game
 	int		line_length;
 }	t_game;
 
-typedef struct s_flag
+typedef struct s_count
 {
-	int	p_flag;
-	int	e_flag;
-	int	c_flag;
-}	t_flag;
+	int	p_count;
+	int	e_count;
+	int	c_count;
+}	t_count;
 
 int		main(int ac, char **av);
 
 void	free_arr2d(char **arr);
 
 int		valid_map_name(char *map);
-int		valid_map_file(char *map, t_game *tool, t_flag *flags);
+int		valid_map_file(char *map, t_game *tool, t_count *counter);
 char	**store_map(char *map, t_game *tool);
 void	count_file_lines(char *map, t_game *tool);
 int		valid_rectangular_size(t_game *tool);
